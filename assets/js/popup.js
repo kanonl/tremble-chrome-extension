330917dfaa775881ff531ca52a19b9751ad89a1f
Kanon Lo <klo@jsls.com>
2018-02-27 10:14:23 -0500

    Reset file

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,18 +1,3 @@
-const b = chrome.extension.getBackgroundPage();
-
-let things = b.fetchChannels();
-
-things.then(kanon => {
-    Promise.all(kanon).then(values => {
-        values.forEach(response => {
-            if (response.ok) {
-                response.json().then(json => {
-                    if (json.streams.length > 0) {
-                        console.log(json.streams[0].channel.name);
-                        document.querySelector(".peep").innerHTML += json.streams[0].channel.name + "<br>";
-                    }
-                });
-            }
-        });
-    });
-});
+(function () {
+    // TODO
+})();
