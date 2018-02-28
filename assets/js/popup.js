5602bb7d11e9fab98082d30053b1c08acfb22e3f
Kanon Lo <klo@jsls.com>
2018-02-28 12:29:55 -0500

    Popup styling and structure

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,6 +1,22 @@
 (function () {
-    document.querySelector("#Peep").innerHTML = Handlebars.templates.popup({
-        "title": "Hello, World!"
+
+    chrome.storage.sync.get("streamList", items => {
+
+        if (items.streamList.length > 0) {
+            document.querySelector("#Peep").innerHTML =
+                Handlebars.templates.popup(items);
+
+            let anchors = document.querySelectorAll("a");
+
+            anchors.forEach(anchor => {
+                anchor.addEventListener("click", event => {
+                    chrome.tabs.create({
+                        "url": anchor.href
+                    });
+                });
+            });
+        }
+
     });
-    chrome.storage.sync.clear();
+
 })();
